ecca6e9566bfebd0e796135143756396aa23631a
unknown <202752792@qq.com>
2020-03-05 20:54:52 +0800

    devloping

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-console.log(1267868763)
-document.querySelector('body').style.background = '#000'
-document.querySelector('body').style.height = '100vh'
-let a = [1, 2, 3, 4, 5];
-let promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve()
-  }, 1000);
-})
-promise().then(() => {
-  console.log(...a)
-})  
+import Util from './util'
+class CEditor {
+  constructor(dom) {
+    this.editor = dom;
+  }
+  init() {
+    this.editor.setAttribute('contenteditable', true);
+  }
+  /**
+   * @param {*} style css list
+   */
+  setStyle(style) {
+    const obj = {
+    }
+    const _oStyle = this.editor.getAttribute('style') || '';
+    let _nStyle = Util.objctToStyle(style);
+    this.editor.setAttribute('style', _oStyle + _nStyle)
+  }
+}
+if (process.env.NODE_ENV === 'development') {
+  let testFun = require('./test');
+  let editor = testFun(CEditor);
+  editor.init()
+  editor.setStyle({
+    // background: '#000',
+    'font-size': '14px',
+    // color: '#fff'
+  });
+}
+export default CEditor
